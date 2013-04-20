2001459e363991f8a63a69446d07123d512bcfde
seshagiriprabhu <seshagiriprabhu@gmail.com>
2013-04-20 10:42:21 +0530

    Adding header file

---
+++ header/header.h
@@ -0,0 +1,8 @@
+/****************Header files*****************/
+#include <stdlib.h>
+#include <stdio.h>
+#include <libvirt/libvirt.h>
+
+
+/*************Function declaration************/
+int checkDomainState(virDomainPtr dom);
